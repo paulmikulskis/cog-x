9d460e0f12656987d2789039084e2ec4d270186d
dreivan <dreivan.orprecio@gmail.com>
2022-10-02 20:07:45 +0800

    Change authentication logic

--- src/server/middleware/authorize.ts
+++ src/server/middleware/authorize.ts
@@ -2,41 +2,31 @@ import { Request, Response, NextFunction } from "express";
 import { respondWith } from "../utils/server_utils";
 import { app } from "firebase-admin/lib/firebase-namespace-api";
 import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
+import { getFirestore } from "firebase-admin/firestore";
 
 const authToken = (firebaseAdmin: app.App) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const authHeader = req.headers["authorization"]?.split(" ");
-    const admin = authHeader && authHeader[0];
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const db = getFirestore();
+      const { uuid, password } = req.body.auth;
 
-    if (admin === "admin") {
-      /* 
-      Logic 1 goes here
+      if (!uuid || !password)
+        return res.send(respondWith(403, `Users not found`));
 
-      */
-      return next();
-    }
-    const token = authHeader && authHeader[1];
+      const userSnapshot = await db.collection("users").doc(uuid).get();
+      if (!userSnapshot.exists)
+        return res.send(respondWith(403, `Users not found`));
 
-    if (!token) {
-      return res.send(respondWith(403, `Invalid Token`));
-    }
+      const user = userSnapshot.data();
 
-    if (!firebaseAdmin)
-      return res.send(respondWith(403, `Authentication Server Error`));
-
-    firebaseAdmin
-      .auth()
-      .verifyIdToken(token)
-      .then((decodedToken: DecodedIdToken) => {
-        /* 
-         Logic 2 goes here
-
-          */
+      if (password === user?.key) {
         return next();
-      })
-      .catch(() => {
-        return res.send(respondWith(403, `Invalid Token`));
-      });
+      } else {
+        return res.send(respondWith(403, `Invalid password credentials`));
+      }
+    } catch {
+      return res.send(respondWith(403, `Authentication Server Error`));
+    }
   };
 };
 
