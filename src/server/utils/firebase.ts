7a8bea5551054fad736c50146a8136433e063ebd
Paul Mikulskis <mikulskisp@gmail.com>
2022-12-02 15:39:17 -0500

    firebase auth in env vars

--- src/server/utils/firebase.ts
+++ src/server/utils/firebase.ts
@@ -1,16 +1,19 @@
 import * as firebaseAdmin from "firebase-admin"
 import { Logger } from "tslog"
+import { config } from "dotenv"
 
 const logger = new Logger()
+config({ path: "base.env" })
+config({ path: ".env", override: true })
 // Initialize Firebase Admin
 const initializeFirebase = () => {
   try {
     const f = firebaseAdmin.initializeApp({
-      credential: firebaseAdmin.credential.cert({
-        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-        projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-      }),
+      credential: firebaseAdmin.credential.cert(
+        JSON.parse(process.env.FIREBASE_SERVICE_ACCT || "{}")
+      ),
+      databaseURL:
+        process.env.FIREBASE_DB_URL || "https://yungsten-f1a69-default-rtdb.firebaseio.com",
     })
     logger.info("Successfully initialized firebase")
     return f
