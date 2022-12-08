0bf6feb1479bb3c13774521ad570231bc90f07ae
Paul Mikulskis <mikulskisp@gmail.com>
2022-12-08 16:39:04 -0500

    scheduleable

--- src/utils/bodies.model.ts
+++ src/utils/bodies.model.ts
@@ -1,18 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { z } from "zod"
+import { HealthCheckBody } from "../server/integrated_functions/healthcheck"
 
 export const exampleFunctionBody = z.object({
   miles: z.number(),
 })
 
-export const exampleFunctionBody2 = z.object({
-  miles: z.number(),
-})
-
+// IMPORTANT:
+// add all functionats that you want to be call via the /scheduler endpoint HERE:
 export const scheduleableFunctions = z.union([
   exampleFunctionBody,
-  exampleFunctionBody2
-  ])
+  HealthCheckBody
+  ]) // .. they will be imported later into the scheduler as acceptable bodies (security measure)
 
 export const SchedulerBody = z.object({
   workflowName: z.string().min(3),
