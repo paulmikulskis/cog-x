717456c37149ae9bd59fce5fb61dec9938c65a86
Paul Mikulskis <mikulskisp@gmail.com>
2023-01-02 21:52:09 -0500

    healthcheck has a return value now

--- src/workers/healthcheck/index.ts
+++ src/workers/healthcheck/index.ts
@@ -5,15 +5,18 @@ export const healthcheck = () => {
   return createIntegratedWorker("healthcheck", async ({ reqBody, _calls }) => {
     // We can't get request headers here
     const endpoint = reqBody.endpoint;
-    const response = await axios.get(endpoint);
 
-    //for now just log response
-    console.log(response.data);
     try {
+      const response = await axios.get(endpoint);
+
+      //for now just log response
+      console.log(response.data);
+      return true;
     } catch (e) {
       console.log(
         `ERROR while trying to send healthcheck ping to '${endpoint}'`
       );
+      return false;
     }
   });
 };
